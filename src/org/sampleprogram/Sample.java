c1b313b8f0e557990193b64a269f86a4cc13209f
sudhar11421 <sudhar11421@gmail.com>
2023-10-05 10:17:41 +0530

    Git Sample

---
+++ src/org/sampleprogram/Sample.java
@@ -0,0 +1,18 @@
+package org.sampleprogram;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		String s = "Application";
+		
+		s.contains(s);
+		
+		System.out.println("Work Done By A");
+		System.out.println("Working on Status");
+		System.out.println("Working fine on Local");
+		
+		
+	}
+	
+}
